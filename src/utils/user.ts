d49a7e6ab439f64a014dd0b351afa8ca1a9b3167
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2025-11-28 11:16:13 +0200

    corrected 11.16

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -2,6 +2,13 @@
 import { connectToDatabase } from "@/lib/mongodb";
 import { User } from "@/models/User";
 
+type UserData = Partial<{
+  userName: string;
+  email: string;
+  password?: string;
+  provider?: string;
+}>;
+
 export async function getUserFromDb(email: string) {
    
     await connectToDatabase(); 
@@ -11,7 +18,7 @@ export async function getUserFromDb(email: string) {
     return await User.findOne({ email: normalizedEmail });
 }
 
-export async function createUserIfNotExists(email: string, data: any) {
+export async function createUserIfNotExists(email: string, data: UserData) {
   const existing = await User.findOne({ email });
   if (!existing) {
     return await User.create(data);
